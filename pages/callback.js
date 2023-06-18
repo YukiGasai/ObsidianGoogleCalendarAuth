351f756425d456e0ba57fd6fa71d08cbc3800219
YukiGasai <r.lindede@googlemail.com>
2023-06-18 20:35:40 +0200

    Remove scope restriction while porting

--- pages/callback.js
+++ pages/callback.js
@@ -9,13 +9,15 @@ export default function Home() {
     const scope = urlParams.get('scope');
     const token = urlParams.get('token');
 
+    console.log(token, scope, state);
+
     if (token && scope) {
       setTimeout(() => {
         // Remove the query params from the url
         window.history.replaceState({}, document.title, "/" + "callback");
 
         // Only allow calendar scope to minimize security risk of a stolen client.
-        if (scope !== "https://www.googleapis.com/auth/calendar") return;
+        // if (scope !== "https://www.googleapis.com/auth/calendar") return;
         window.location = `obsidian://googleLogin?token=${token}`;
       }, 200)
       return;
@@ -29,7 +31,7 @@ export default function Home() {
       if (!state || !scope) return;
 
       // Only allow calendar scope to minimize security risk of a stolen client.
-      if (scope !== "https://www.googleapis.com/auth/calendar") return;
+      // if (scope !== "https://www.googleapis.com/auth/calendar") return;
       window.location = `obsidian://googleLogin?${params}`;
     }, 200)
 
