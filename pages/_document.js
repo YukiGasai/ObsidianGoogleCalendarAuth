2655f6f921a36ae589f2d55c7a61c1c0277d2689
YukiGasai <r.lindede@googlemail.com>
2023-06-19 16:23:17 +0200

    Added google validation

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="google-site-verification" content="Of2zfK-3x1k523P4zrmEear2AIql4XL5jNlcqC44q8o" />
       <body>
         <Main />
         <NextScript />
