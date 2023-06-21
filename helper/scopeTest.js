a9a9362b7efa3673db5be97c6182cf53c12327d5
YukiGasai <r.lindede@googlemail.com>
2023-06-21 19:23:07 +0200

    Allow more scopes for older clients

--- helper/scopeTest.js
+++ helper/scopeTest.js
@@ -3,8 +3,17 @@ export const scopeTest = (scopesString) => {
     const scopes = scopesString.split(" ");
 
     // Allow only calendar scopes
-    // Allow /calendar scope for compatibility with old clients
-    const allowedScopes = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar.readonly"];
+    // Allow /calendar scope and other non calendar scopes for compatibility with older clients
+    const allowedScopes = [
+        "https://www.googleapis.com/auth/calendar",
+        "https://www.googleapis.com/auth/calendar.events",
+        "https://www.googleapis.com/auth/calendar.readonly",
+        "email",
+        "https://www.googleapis.com/auth/userinfo.email",
+        "profile",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid",
+    ];
 
     return scopes.every(scope => allowedScopes.indexOf(scope) > -1);
 }
